cb32026c11efffc544decfce058f7dea774bd0a0
kaipov24 <kaipovich24@gmail.com>
2026-07-02 17:30:40 +0600

    Sitemap v3

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,12 +3,10 @@ import type { MetadataRoute } from "next";
 export const dynamic = "force-static";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://kaipov24.github.io/arsenal-countdown";
-
   return [
     {
-      url: `${baseUrl}/`,
-      lastModified: new Date("2026-06-30"),
+      url: "https://kaipov24.github.io/arsenal-countdown/",
+      lastModified: new Date("2026-07-02"),
       changeFrequency: "daily",
       priority: 1,
     },
